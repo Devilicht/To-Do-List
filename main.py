c0ce72d2360f54a6e1f49234d70757d325e1f6c9
unknown <hollow66a@gmail.com>
2023-03-26 13:09:24 -0300

    finalized

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import Flask,request,jsonify
 from werkzeug.security import generate_password_hash, check_password_hash
 import psycopg2
-from datetime import datetime
+from datetime import datetime,timedelta
+import jwt
+ 
 
 app = Flask(__name__)
 
@@ -14,79 +16,93 @@ conn = psycopg2.connect(
 )
 cur = conn.cursor()
 
-@app.route('/getLoggedUserId', methods=['GET'])
-async def getLoggedUserId():
-    userEmail = request.headers.get('Authorization')
-    cur.execute('SELECT id FROM users WHERE email=%s', (userEmail,))
-    result = cur.fetchone()
-    
-    if result is not None:
-        return jsonify({'user_id': result[0]}), 200
-    else:
-        return jsonify({'error': 'Unauthenticated user.'}), 401
+app.config['SECRET_KEY'] = 'mySecretKey'
 
+def generate_token(user_id):
+    payload = {'user_id': user_id, 'exp': datetime.utcnow() + timedelta(minutes=30)}
+    token = jwt.encode(payload, app.config['SECRET_KEY'])
+    return token
 
-@app.route('/users', methods = ['POST'])
-async def users():
-    email= request.json['email']
+def decode_token(token):
+    try:
+        decoded_token = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
+        return decoded_token
+    except jwt.exceptions.DecodeError:
+        return None
+
+@app.route('/users', methods=['POST'])
+def auth():
+    email = request.json['email']
     password = request.json['password']
 
-    cur.execute('SELECT password FROM users WHERE email=%s', (email,))
+    cur.execute('SELECT user_id, password FROM users WHERE email=%s', (email,))
     result = cur.fetchone()
     conn.commit()
-    cur.close()
 
-    if result is not None and check_password_hash(result[0], password):
-        return jsonify({'message': 'Successful login!'})
+    if result is not None and check_password_hash(result[1], password):
+        token = generate_token(result[0])
+        return jsonify({'message': 'Successful login!', 'token': token}), 200
     else:
-         return jsonify({'message': 'Incorrect user or password'})
+        return jsonify({'message': 'Incorrect user or password'}), 401
+
+@app.route('/getLoggedUserId', methods=['GET'])
+def getLoggedUserId():
+    token = request.headers.get('Authorization')
+    decoded_token = decode_token(token)
+
+    if decoded_token is not None:
+        user_id = decoded_token['user_id']
+        return jsonify({'user_id': user_id}), 200
+    else:
+        return jsonify({'error': 'Invalid token'}), 401
     
 
-@app.route('/users/register', methods =['POST']) 
-async def register():
+@app.route('/users/register', methods=['POST'])
+def register():
     name = request.json['name']
     email = request.json['email']
     password = request.json['password']
     confirm_password = request.json['confirm_password']
 
     if password != confirm_password:
-        return jsonify({'message': 'passwords dont match'})
-    
+        return jsonify({'message': 'Passwords do not match'})
+
     hashed_password = generate_password_hash(password)
 
-    cur.execute('INSERT INTO users (name,email, password) VALUES (%s,%s,%s)',(name,email, hashed_password))
+    cur.execute('INSERT INTO users (name,email, password) VALUES (%s,%s,%s)', (name, email, hashed_password))
     conn.commit()
-    cur.close()   
-    return jsonify({'message': 'account added'})
 
+    return jsonify({'message': 'Account added'})
 
-@app.route('/users/myTasks', methods=['POST'])
-async def createTask():
+@app.route('/users/myTasks/<int:user_id>/<int:task_id>', methods=['GET'])
+def readTasks(user_id,task_id):       
     user_id = getLoggedUserId()
+    cur.execute('SELECT * FROM tasks WHERE task_id=%s AND user_id=%s', (task_id,user_id,))
+    tasks = cur.fetchall()
+    return jsonify(tasks)
+
+@app.route('/users/myTasks/<int:user_id>', methods=['POST'])
+def createTask(user_id):
+    user_id = request.headers.get('Authorization') 
+    try:
+        decoded_token = jwt.decode(user_id, app.config['SECRET_KEY'], algorithms=['HS256']) 
+        user_id = decoded_token['user_id'] 
+    except jwt.exceptions.DecodeError:
+        return jsonify({'error': 'Invalid token'}), 401
     
     title = request.json['title']
-    description = request.json.get('description', '')
+    description = request.json.get('description','')
     
     cur.execute('INSERT INTO tasks (title, description, user_id) VALUES (%s, %s, %s)', (title, description, user_id))
     conn.commit()
 
     return jsonify({'message': 'Task created successfully'})
 
-
-
-@app.route('/users/int:user_id/myTasks', methods=['GET'])
-async def readTasks():       
-    user_id = getLoggedUserId()
-    cur.execute('SELECT * FROM tasks WHERE user_id=%s', (user_id,))
-    tasks = cur.fetchall()
-    return jsonify(tasks)
-
-
-@app.route('/users/myTasks/<int:task_id>', methods=['PUT'])
-async def update_task(task_id):
+@app.route('/users/myTasks/<int:user_id>', methods=['PUT'])
+def update_task(task_id):
     user_id = getLoggedUserId()
     
-    cur.execute('SELECT * FROM tasks WHERE id=%s AND user_id=%s', (task_id, user_id))
+    cur.execute('SELECT * FROM tasks WHERE task_id=%s AND user_id=%s', (task_id, user_id))
     task = cur.fetchone()
     if task is None:
         return jsonify({'error': 'Task not found or does not belong to logged user'})
@@ -95,13 +111,13 @@ async def update_task(task_id):
     description = request.json.get('description', task[2])
     is_done = request.json.get('is_done', task[3])
 
-    cur.execute('UPDATE tasks SET title=%s, description=%s, is_done=%s WHERE id=%s', (title, description, is_done, task_id))
+    cur.execute('UPDATE tasks SET title=%s, description=%s, is_done=%s WHERE tasks_id=%s', (title, description, is_done, task_id))
     conn.commit()
 
     return jsonify({'message': 'Task updated'})
 
-@app.route('/users/myTasks/<int:task_id>', methods=['DELETE'])
-async def delete_task(task_id):
+@app.route('/users/myTasks/<int:user_id>', methods=['DELETE'])
+def delete_task(task_id):
     user_id = getLoggedUserId()
     
     cur.execute('SELECT * FROM tasks WHERE id=%s AND user_id=%s', (task_id, user_id))
