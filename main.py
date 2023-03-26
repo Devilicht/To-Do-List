cd1d42404bedf14dc332622e9f98f2dac866a281
unknown <hollow66a@gmail.com>
2023-03-26 10:31:51 -0300

    Finalized

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def getLoggedUserId():
     if result is not None:
         return jsonify({'user_id': result[0]}), 200
     else:
-        return jsonify({'error': 'Usuário não autenticado.'}), 401
+        return jsonify({'error': 'Unauthenticated user.'}), 401
 
 
 @app.route('/users', methods = ['POST'])
